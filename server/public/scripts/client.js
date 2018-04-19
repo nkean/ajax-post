3e15752d06840e7d819ec7c37830acae4b506c3d
nkean <nathankean2@gmail.com>
2018-04-19 12:01:26 -0500

    add the records to a table on the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,23 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery had been loaded');
+    displayRecords();
+}
+
+function displayRecords() {
+    $.ajax({
+        method: 'GET',  // could also use type: , but it breaks in angular?
+        url: '/records'
+    })
+        .then(function (response) {
+            response.forEach(function (record) {
+                let formattedCost = record.cost.toLocaleString('en', { style: 'currency', currency: 'USD' }).slice(0, -3);
+                $('#recordList').append(`<tr>
+                                            <td>${record.title}</td>
+                                            <td>${record.artist}</td>
+                                            <td>${record.year}</td>
+                                            <td>${formattedCost}</td>
+                                         </tr>`);
+            });
+        });
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+// this will live in the database shortly VVV
+const recordCollection = require('./modules/record-collection');
+
 app.use(express.static('server/public'));
+
+// routes (get, post, put delete requests)
+
+app.get('/records', (req, res) => {
+    res.send(recordCollection);
+});
 
 app.listen(PORT, () => {
     console.log(`up and running on port: ${PORT}`);
